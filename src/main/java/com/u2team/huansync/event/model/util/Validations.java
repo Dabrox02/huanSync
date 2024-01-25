7f31b9d3fb132217fd1425cdc409d3e917206fad
KazeVentum <kazeventum@gmail.com>
2024-01-25 10:36:42 -0500

    docs: :memo: Add final Comments in equipment and Validations files

--- src/main/java/com/u2team/huansync/event/model/util/Validations.java
+++ src/main/java/com/u2team/huansync/event/model/util/Validations.java
@@ -11,13 +11,33 @@ import java.time.temporal.ChronoUnit;
 /**
  *
  * @author Cristina Jerez Salas
+ * 
  */
 public class Validations {
     
+
+    /**
+    * Checks if a date is between an initial date and a final date, within a specified range of days.
+    *
+    * @param initialDate the initial date
+    * @param finalDate the final date
+    * @param minDays the minimum number of days allowed between the initial and final dates
+    * @param maxDays the maximum number of days allowed between the initial and final dates
+    * @return true if the date is between the initial and final dates within the specified range of days, false otherwise
+    */
     public static boolean dateBetween(LocalDate initialDate, LocalDate finalDate, int minDays, int maxDays){
         long difDate = ChronoUnit.DAYS.between(initialDate, finalDate);
         return !(difDate < minDays || difDate > maxDays);
     }
+
+    /**
+     * Counts the number of occurrences of a specific value in a column of a table.
+     *
+     * @param table the name of the table
+     * @param column the name of the column
+     * @param value the value to search for in the column
+     * @return the number of occurrences of the value in the column, or -1 if an error occurs
+     */
     
     public static int counterRepeated(String table, String column, String value){
         Operations.setConnection(BDConnection.MySQLConnection());
